8d58bcf2b9f51c6797e0d641d73b8fa1bf420d2a
Zurie Baharom <rootradical@gmail.com>
2026-07-09 10:45:36 +0800

    Remove sensitive config files from Git tracking

--- config/ubiquiti_ap_access.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'controller_url' => 'https://10.14.49.10:8443/manage/default/devices',
-    'inform_url' => 'http://10.14.49.10:8080/inform',
-    'username' => 'kmperlis',
-    'password' => 'bqHt0yKsv2GHzF8y',
-];
